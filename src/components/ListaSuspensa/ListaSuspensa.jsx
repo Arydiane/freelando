3011b05d236f61f3abb10bb0d3dfeee9b2463e3e
Arydiane <arydianejardim@gmail.com>
2023-07-22 17:58:20 -0300

    atualizando componente lista suspensa adiciona tratamento das teclas tab e esc

--- src/components/ListaSuspensa/ListaSuspensa.jsx
+++ src/components/ListaSuspensa/ListaSuspensa.jsx
@@ -81,6 +81,9 @@ export const ListaSuspensa = ({ titulo, opcoes }) => {
                     if (focoAntigo === null) {
                         return 0;
                     }
+                    if (focoAntigo === (opcoes.length - 1)) {
+                        return opcoes.length - 1
+                    }
                     return focoAntigo +=1
                 })
                 break;
@@ -99,6 +102,13 @@ export const ListaSuspensa = ({ titulo, opcoes }) => {
                 alternarVisibilidade(false)
                 setOpcaoSelecionada(opcoes[opcaoFocada])
                 break;
+            case 'Tab': 
+            case 'Escape': 
+                evento.preventDefault(); 
+                setOpcaoFocada(null)
+                alternarVisibilidade(false)
+                break;
+              
             default:
                 break;
         }
