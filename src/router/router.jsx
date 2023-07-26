2a05500ee3da7fbf199ac48cd6a8f51b0093b3d7
Arydiane <arydianejardim@gmail.com>
2023-07-25 21:02:23 -0300

    atualiza rotas

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,7 +2,9 @@ import { createBrowserRouter } from "react-router-dom";
 import SelecaoCliente from "../pages/cadastro/SelecaoCliente";
 import LayoutBase from "../pages/LayoutBase";
 import LayoutBaseCadastro from "../pages/cadastro/LayoutBaseCadastro";
-
+import Interesses from "../pages/cadastro/Interesses";
+import DadosPessoais from "../pages/cadastro/DadosPessoais"
+import Concluido from "../pages/cadastro/Concluido";
 
 export const router = createBrowserRouter([
     {
@@ -18,16 +20,16 @@ export const router = createBrowserRouter([
                         element: <SelecaoCliente />
                     },
                     {
-                        path: 'cliente',
-                        element: <h1>Interesses</h1>
+                        path: 'interesses',
+                        element: <Interesses />
                     },
                     {
                         path: 'dados-pessoais',
-                        element: <h1>Dados pessoais</h1>
+                        element: <DadosPessoais />
                     },
                     {
                         path: 'concluido',
-                        element: <h1>Concluido</h1>
+                        element: <Concluido />
                     }
                 ]
             }
