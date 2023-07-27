93b6c381f8f0fc25dab263f6620f0873e5945e4b
Arydiane <arydianejardim@gmail.com>
2023-07-27 19:41:23 -0300

    refatora componente Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,14 +3,14 @@ import styled from '@emotion/styled'
 const DivEstilizada = styled.div`
     padding: ${props => props.theme.espacamentos.l};
     border-radius: ${props => props.theme.espacamentos.s};
-    border: 1px solid; 
-    border-color:  ${props => props.theme.cores.primarias.a};
-    background: ${props => props.theme.cores.secundarias.a};
+    border: ${props => props.comBorda ? '1px solid' : 'none'}; 
+    border-color:  ${props => props.variante === 'primaria' ? props.theme.cores.primarias.a : props.theme.cores.primarias.c};
+    background: ${props => props.variante === 'primaria' ? props.theme.cores.secundarias.a : props.theme.cores.secundarias.c};
 `
 
-export const Card = ({ children }) => {
+export const Card = ({ children, comBorda = true, variante = 'primaria' }) => {
     return (
-        <DivEstilizada>
+        <DivEstilizada comBorda={comBorda} variante={variante} >
             {children}
         </DivEstilizada>
     )
