87de24d56629169eee9a647f3e33b0074133a0d0
Arydiane <arydianejardim@gmail.com>
2023-07-28 18:45:29 -0300

    atualiza estilização componente estilos e link

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled'
 
-const LinkPrimario = styled.a`
+const LinkPrimario = styled.span`
     cursor: pointer;
     font-weight: 400;
     font-size: 20px;
@@ -12,7 +12,7 @@ const LinkPrimario = styled.a`
     }
 `
 
-const LinkSecundario = styled.a`
+const LinkSecundario = styled.span`
     cursor: pointer;
     font-weight: 400;
     font-size: 16px;
